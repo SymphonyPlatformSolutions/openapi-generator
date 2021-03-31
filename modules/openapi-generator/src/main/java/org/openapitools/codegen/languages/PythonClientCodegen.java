b39b50ea82c6b6da1fa0ae45f67acbd83d0e072e
Elias Croze <elias.croze@symphony.com>
2021-03-31 16:40:26 +0200

    Removed import

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/PythonClientCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/PythonClientCodegen.java
@@ -17,7 +17,6 @@
 package org.openapitools.codegen.languages;
 
 import com.google.common.collect.Sets;
-import com.sun.imageio.plugins.common.SingleTileRenderedImage;
 import io.swagger.v3.core.util.Json;
 import io.swagger.v3.oas.models.media.*;
 import io.swagger.v3.oas.models.media.ArraySchema;
